c459e4bdc9837c6edbd02d0075dcc7f6dc009649
Jillian Fusco <fuscojillian@gmail.com>
2024-08-16 15:23:24 -0400

    sucessfully rendering on page

--- script.js
+++ script.js
@@ -14,21 +14,26 @@ window.addEventListener("load", function() {
 
     let listedPlanetsResponse;
 
-    listedPlanetsResponse.then(function (result) {
-        listedPlanets = result;
-        console.log(listedPlanets);
-    }).then(function () {
-        console.log(listedPlanets);
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
 
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-    });
+    // });
 
     let form = document.getElementById("launchForm"); 
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
-        let pilot = document.querySelector("input[name=pilotName]").value
-        // do the rest
+
+        let list;
+        let pilot = document.querySelector("input[name=pilotName]").value;
+        let copilot = document.querySelector("input[name=copilotName").value;
+        let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
+        let cargoMass = document.querySelector("input[name=cargoMass]").value;
+        
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoMass);
         
     });
